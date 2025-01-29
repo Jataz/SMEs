9e53aa133f5f24d1ffd3b09144de1778f2f69753
root <root@vmi2424601.contaboserver.net>
2025-01-29 11:20:44 +0100

    Adding Redis to the settings

--- core/settings.py
+++ core/settings.py
@@ -189,3 +189,15 @@ LOGOUT_REDIRECT_URL = '/login'
 USE_X_FORWARDED_HOST = True
 USE_X_FORWARDED_PORT = True
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SOCKET_TIMEOUT": 5,
+            "SOCKET_CONNECT_TIMEOUT": 5,
+        }
+    }
+}
+
